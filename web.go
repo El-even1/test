977695fb6a8e9da5222288013ffb18bf59a4f2e5
ele <123@qq.com>
2026-04-22 17:17:12 +0800

    test

--- web.go
+++ web.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>简单的 Go Web 页面</title>
+    <title>简单的 Go1 Web 页面</title>
     <style>
         body {
             font-family: Arial, sans-serif;
